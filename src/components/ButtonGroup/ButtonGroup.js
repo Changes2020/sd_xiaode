7271b1ca519f157171d18ff70b806bd933116d8f
wangjing <1915093132@qq.com>
2018-07-26 19:36:19 +0800

    button组件代码提交

---
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -0,0 +1,58 @@
+
+/*ButtonGroup组件参数介绍:一共7个参数，两个必传参数
+*
+* dataSource{必传 Obj}:数据源，对象结构:{data:[{id:xx,name"'xx'}.....],...},里面有要map遍历的data数据源，data为数组格式，里面为对象，对象内的数据key值为id和name，父组件需要处理好传入。
+* dataReturnFun{必传 Funciton}:父组件需要传入点击对应button返回时触发接受数据的function，返回数据为（item,index）
+* id{非必传 Number}:传入默认选中button的id值
+* isSelectFirst{非必传 Boolean}:若父组件未传入id属性，则判断是否有该属性，有的话默认第一个button被选中，没有则没有button被选中。
+* spanFunction{非必传 Function}:传入该属性，则父组件确定button里面文字展示内容，没有则默认button里面的文字只显示name,
+* btnClass{非必传 Obj}:未被选中button样式，对象格式
+* btnSelectedClass{非必传 Obj}:被选中button样式，对象格式
+*
+* */
+import React, {Component} from 'react';
+import styles from './ButtonGroup.less';
+import {Button} from 'antd-mobile';
+
+class ButtonGroup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  };
+
+  buttonListItem(dataSource=null,dataReturnFun=null,id=null,isSelectFirst=null,spanFunction=null,btnClass=null) {
+    console.log('进入到渲染button组件',dataSource,dataReturnFun,id,isSelectFirst,spanFunction,btnClass)
+    const self = this;
+    const data = !dataSource?{data:[]}:!dataSource.data?{data:[]}:dataSource.data
+    if (!dataReturnFun||typeof(dataReturnFun)!=='function') {
+      console.warn('未传入dataReturnFun方法或传入的非function');
+    }
+    if (!spanFunction&&typeof(spanFunction)!=='function') {
+      console.warn('传入的spanFunction非function');
+    }
+    let liList = data.map(function (item, index) {
+      return (
+        <Button
+          className={!id?(!isSelectFirst?styles.btnStyle:(index === 0 ? styles.btnSelected : styles.btnStyle)):(item.id === id ? styles.btnSelected : styles.btnStyle)}
+          key={index}
+          onClick={() => (dataReturnFun(item,index))}
+        >{!spanFunction?<span>{item.name}</span>:spanFunction(item,id)}
+        </Button>
+      )
+    })
+    return (liList)
+  }
+
+  render() {
+    const {dataSource=null,dataReturnFun=null,id=null,isSelectFirst=null,spanFunction=null,btnClass=null} = this.props
+    console.log('刚进入页面时候',dataSource,dataReturnFun,id,isSelectFirst,spanFunction,btnClass)
+    return (
+      <div>
+        {this.buttonListItem(dataSource,dataReturnFun,id,isSelectFirst,spanFunction,btnClass)}
+      </div>
+    );
+  }
+}
+
+export default ButtonGroup;
--- src/routes/Demention/Demention.jsx
+++ src/routes/Demention/Demention.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { connect } from 'dva';
-// import Loading from 'components/Loading/Loading';
-// import { Toast } from 'antd-mobile';
 import {assignUrlParams} from 'utils/routerUtils'
+import styles from './Demention.less';
+import ButtonGroup from '../../components/ButtonGroup/ButtonGroup';
+
+  //ButtonGroup组件测试
+  const btnContainerTest = (obj = null, fnClick = null ,id = null,spanFun=null) => (
+    <div className={styles.btnContainer}>
+      <ButtonGroup
+        dataSource = {obj}
+        dataReturnFun={(item,index) => (fnClick(item,index))}
+        id={id}
+        isSelectFirst={true}
+        // spanFunction={(item,id) => (spanFun(item,id))}
+        // btnClass='btnStyle'
+      />
+    </div>
+  );
+
 
  class Demention extends React.Component{
    constructor(props){
@@ -18,22 +33,48 @@ import {assignUrlParams} from 'utils/routerUtils'
         dateType: 3,       //1:周均,2:月均,3:自定义
         filteKeyID: null,  //登录用户id
         userId: null,
+        dementionId:4,
       }
      }
      this.state=assignUrlParams(initState,urlParams)
    }
    componentDidMount(){
-    
+
    }
+  // 父组件控制butonn组内容显示
+   spanFun(item,id){
+     return (
+       <span>
+        <span className={item.id === id ? styles.spanIdSelected : styles.spanIdStyle}>{item.id}</span>
+        <span className={item.id === id ? styles.spanNameSelected : styles.spanNameStyle}>{item.name}</span>
+      </span>
+     )
+   }
+
+   //点击button触发的请求chart和table接口函数
+   fnClickGroupButton(groupId,index) {
+     console.log('父组件的button',groupId,index)
+     const dementionId = groupId.id
+     this.setState({
+       dementionId: dementionId,
+     })
+   }
+
   render(){
     const {paramsObj}=this.state;
+    const dataSource = {data:[
+      {name: "直播", id: 4, rawDataDes: "直播（小时）"},
+        {name: "预估分", id: 32, rawDataDes: "预估分"},
+        {name: "主帖", id: 33, rawDataDes: "主帖"},
+        {name: "跟帖", id: 34, rawDataDes: "跟帖"},
+        {name: "优质帖", id: 35, rawDataDes: "优质帖"}]}
+
     return(
-<div>
     <div>
-    {JSON.stringify(paramsObj)}
-    </div>
- 
-    {/* <Loading/>  */}
+      <div>
+      {JSON.stringify(paramsObj)}
+      </div>
+      {btnContainerTest(dataSource, this.fnClickGroupButton.bind(this),this.state.dementionId,this.spanFun.bind(this))}
     </div>
     )
   }
