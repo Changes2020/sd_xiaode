bf985c916c7d9e17cb38a4f0d0fa8c8a23219f37
mzh <manzhenhua@sunlands.com>
2018-07-31 10:52:33 +0800

    修改权限

--- src/utils/authority.jsx
+++ src/utils/authority.jsx
@@ -1,8 +1,8 @@
 import { getItem } from 'utils/localStorage';
 /*
-* 权限判断 1  
+* 权限判断 1
 *  获取用户userId,如果没有则表示无权限
-*         
+*
 */
 export function getAuthority() {
   const chartInfo = getItem('userInfo') || {};
@@ -10,7 +10,7 @@ export function getAuthority() {
   const { isExpries = false } = chartInfo;
   value = chartInfo.value || {};
   const { userId = null } = value;
-  if (userId && isExpries === false) {
+  if (userId && !isExpries) {
     return userId;
   } else {
     return false;
