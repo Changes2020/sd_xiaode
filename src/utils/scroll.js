45f54e3aaca2cef000e492bfd9b03f9db2d97fbd
mzh <manzhenhua@sunlands.com>
2018-08-06 15:19:55 +0800

    a

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -6,16 +6,19 @@
 export function scroll(currentY, targetY) {
   // 计算需要移动的距离
   const needScrollTop = targetY - currentY;
+  const stepNum = 15;
   let _currentY = currentY;
-  setTimeout(() => {
+  const time = setTimeout(() => {
     // 一次调用滑动帧数，每次调用会不一样
-    _currentY += Math.ceil(needScrollTop / 15);
+    _currentY += Math.ceil(needScrollTop / stepNum);
+
     window.scrollTo(_currentY, currentY);
     // 如果移动幅度小于十个像素，直接移动，否则递归调用，实现动画效果
-    if (needScrollTop > 10 || needScrollTop < -10) {
+    if (needScrollTop > stepNum || needScrollTop < -stepNum) {
       scroll(_currentY, targetY);
     } else {
       window.scrollTo(_currentY, targetY);
+      clearTimeout(time);
     }
   }, 1);
 }
