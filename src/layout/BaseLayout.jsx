7dc91e25cfd3fe09c74263c37c56022bca559d7b
mzh <manzhenhua@sunlands.com>
2018-08-06 17:43:57 +0800

    xiugai

--- src/layout/BaseLayout.jsx
+++ src/layout/BaseLayout.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Redirect, Switch, Route, routerRedux } from 'dva/router';
+import { Switch, Route, routerRedux } from 'dva/router';
 import { getRoutes } from '../utils/routerUtils';
-import { getItem } from '../utils/localStorage';
 import { getAuthority } from '../utils/authority';
 
 class BaseLayout extends React.Component {
   componentWillMount() {
     this.getUserInfo();
-    this.checkoutOrgmap();
-    this.getDisAbleDate();
   }
-  getDisAbleDate = () => {
-    const { isTimeData } = this.props;
-    if (isTimeData) {
-      return;
-    }
-    this.props.dispatch({
-      // 此数据在其他请求中会用到,所以放在will里面请求
-      type: 'index/getDisAbleDate',
-      payload: {},
-    });
-  };
-
   getUserInfo = () => {
     const userId = getAuthority();
     const { isLogin } = this.props;
@@ -37,25 +22,6 @@ class BaseLayout extends React.Component {
       }
     } else {
       this.props.dispatch(routerRedux.push('/exception/403'));
-    }
-  };
-  checkoutOrgmap = () => {
-    const { isOrgMap } = this.props;
-    if (isOrgMap) {
-      return;
-    }
-    // 此方法检测组织结构是否过期
-    const store = getItem('allOrgMap');
-    const { value, isExpries } = store;
-    if (isExpries || !value) {
-      this.props.dispatch({
-        type: 'index/getOrgMap',
-      });
-    } else {
-      this.props.dispatch({
-        type: 'index/saveOrgMap',
-        payload: { code: 2000 },
-      });
     }
   };
   render() {
@@ -73,7 +39,6 @@ class BaseLayout extends React.Component {
               redirectPath="/exception/403"
             />
           ))}
-          <Redirect exact from="/" to="/indexPage" />
         </Switch>
       </div>
     );
@@ -84,6 +49,4 @@ export default connect(({ index, loading }) => ({
   index,
   loading,
   isLogin: index.isLogin,
-  isTimeData: index.isTimeData,
-  isOrgMap: index.isOrgMap,
 }))(BaseLayout);
