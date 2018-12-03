584fd0997d1fa3e2e34f945b8e5edc46ac939dc2
lyx <123>
2018-12-03 10:20:01 +0800

    buttonGroupPro优化

--- src/components/ButtonGroupPro/ButtonGroupPro.js
+++ src/components/ButtonGroupPro/ButtonGroupPro.js
@@ -1,8 +1,8 @@
-/* ButtonGroup组件参数介绍:一共7个参数，两个必传参数
+/* ButtonGroup组件参数介绍:一共6个参数，两个必传参数
 *
 * dataSource{必传 Obj}:数据源，对象结构:{data:[{id:xx,name":'xx',isFirst: true}.....],...},里面有要map遍历的data数据源，data为数组格式，里面为对象，对象内的数据key值为id,name,isFirst(是否第一名)，父组件需要处理好传入。
 * dataReturnFun{必传 Funciton}:父组件需要传入点击对应button返回时触发接受数据的function，返回数据为（item,index）
-* selectedIdList{非必传 Array}:传入默认选中button的id数组
+* selectedIdList{非必传 Array}:传入默认选中button的id数组，eg:[id1,id2,id3]
 * spanFunction{非必传 Function}:传入该属性，则父组件确定button里面文字展示内容，没有则默认button里面的文字只显示name,
 * btnClass{非必传 Obj}:未被选中button样式，对象格式
 * btnSelectedClass{非必传 Obj}:被选中button样式，对象格式
