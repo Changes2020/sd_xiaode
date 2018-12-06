aebbddb80140b5fd9ad095f96c32320d8971ff35
lixioajing <lixiaojing01@sunlands.com>
2018-12-06 15:24:45 +0800

    增加权限

--- src/routes/ScorePK/_scoreDetail.js
+++ src/routes/ScorePK/_scoreDetail.js
@@ -3,12 +3,23 @@ import { Icon } from 'antd-mobile';
 import styles from './_score.less';
 
 class RenderItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  jump2Data = (arrowShow, type, data) => {
+    if (arrowShow) {
+      this.props.setRouteUrlParams('/demention', {
+        familyType: data.originObj.familyType, // 0:自考，1:壁垒，2:孵化器
+        titleName: data.originObj.orgName, // 学院名字
+        groupId: data.originObj.id, // 学院id
+        groupName: '学分均分',
+        type, // 正面均分/负面均分
+        dementionId: data.id, // 直播id
+        buttonName: data.dimensionName, // 直播名字
+      });
+    } else {
+      console.warn('无权限查看');
+    }
+  };
   // 渲染3级数据
-  renderList = (data, index,arrowShow,type) => {
+  renderList = (data, index, arrowShow, type) => {
     const liList = data.map(item => {
       return (
         <div
@@ -16,14 +27,20 @@ class RenderItem extends React.Component {
           className={`${styles.dataCss} ${data.length > 2 ? styles.width_3 : styles.width_2}`}
         >
           <span className={styles.u_unitScore}>{item.childNode[index].avgScore.toFixed(2)}</span>
-          <Icon type="right" className={type===1?styles.echartsIcon:styles.echartsIcon2} size="lg" isShow={arrowShow} />
+          <Icon
+            type="right"
+            className={type === 1 ? styles.echartsIcon : styles.echartsIcon2}
+            size="lg"
+            isShow={arrowShow}
+            onClick={this.jump2Data.bind(this, arrowShow, type, item.childNode[index])}
+          />
         </div>
       );
     });
     return <div className={styles.u_rightCss}>{liList}</div>;
   };
   render() {
-    const { paramsObj = [] ,arrowShow=false,type=1} = this.props;
+    const { paramsObj = [], arrowShow = false, type = 1 } = this.props;
 
     return (
       <div className={`${styles.m_container} ${styles.m_regContain}`}>
@@ -32,7 +49,7 @@ class RenderItem extends React.Component {
               return (
                 <div key={item.id} className={`${styles.u_colCss} ${styles.u_regColCss}`}>
                   <span className={styles.u_leftCss}>{item.dimensionName}</span>
-                  {this.renderList(paramsObj, index,arrowShow,type)}
+                  {this.renderList(paramsObj, index, arrowShow, type)}
                 </div>
               );
             })
