980f77c2c1e428e88d6571f3166b268669142de2
lyx <123>
2018-12-06 18:21:06 +0800

    增加pk 数据排序；完善排序方法，将空内容放到最后

--- src/utils/sortChineseWord.js
+++ src/utils/sortChineseWord.js
@@ -1,8 +1,19 @@
+/* eslint-disable no-unused-expressions */
 export function SortChanseData(data, key = 'name') {
   const newArr = data.sort((a, b) => {
     const num = a[key].localeCompare(b[key]);
     const aa = num > 0 ? 1 : num < 0 ? -1 : 0;
     return aa;
   });
-  return newArr;
+
+  let result = [];
+  for (let i = 0; i < newArr.length; i += 1) {
+    if (String(newArr[i][key]).length > 0) {
+      result = newArr.slice(i);
+      result = result.concat(newArr.slice(0, i));
+      break;
+    }
+  }
+
+  return result;
 }
