87f9faab4cd62249478bc04bc8b7608112fccd5c
wangjing <1915093132@qq.com>
2019-01-24 10:17:31 +0800

    给时间一个默认值

--- src/components/TableItem/DetailTableItem.js
+++ src/components/TableItem/DetailTableItem.js
@@ -12,7 +12,7 @@ class DetailTableItem extends React.Component {
       dialogData: {},
     };
   }
-  checkDetal = (titleOne, titleTwo,titleFour) => {
+  checkDetal = (titleOne='2019-01-25', titleTwo,titleFour) => {
     this.setState({
       isShowModal: true,
       dialogData: {
