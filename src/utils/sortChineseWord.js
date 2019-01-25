0e6b727655a95a987db240c2a95406032d3e643e
lixioajing <lixiaojing01@sunlands.com>
2019-01-25 17:01:50 +0800

    排序优化

--- src/utils/sortChineseWord.js
+++ src/utils/sortChineseWord.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-expressions */
 export function SortChanseData(data, key = 'name') {
   const newArr = data.sort((a, b) => {
-    const num = a[key].localeCompare(b[key]);
-    const aa = num > 0 ? 1 : num < 0 ? -1 : 0;
-    return aa;
+    if (!a[key].localeCompare) {
+      const num = a[key].localeCompare(b[key]);
+      const aa = num > 0 ? 1 : num < 0 ? -1 : 0;
+      return aa;
+    } else {
+      return b - a;
+    }
   });
-
   let result = [];
   for (let i = 0; i < newArr.length; i += 1) {
     if (String(newArr[i][key]).length > 0) {
@@ -14,6 +17,5 @@ export function SortChanseData(data, key = 'name') {
       break;
     }
   }
-
   return result;
 }
